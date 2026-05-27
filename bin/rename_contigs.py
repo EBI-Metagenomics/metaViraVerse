5648ae9fcd38b6f13bc5297155befe40502c51fc
Ekaterina Sakharova <kates@ebi.ac.uk>
2026-05-27 14:12:42 +0100

    WIP preprocessing

--- bin/rename_contigs.py
+++ bin/rename_contigs.py
@@ -6,6 +6,7 @@ import argparse
 import csv
 import os
 import fileinput
+import re
 
 
 def input_args():
@@ -24,6 +25,9 @@ def input_args():
     )
     parser.add_argument(
         "-p", "--prefix", help="Prefix that would be included to header <prefix><digit>", required=False
+    )
+    parser.add_argument(
+        "-k", "--keep-viral-identifier", help="Keep info after | in new name", action='store_true'
     )
     args = parser.parse_args()
     return args
@@ -46,12 +50,13 @@ def parse_attrs(attrs_str: str) -> tuple[dict[str, str], list[str]]:
     return attrs, order
 
 
-def rename_fasta(input_fasta, mapfilename, prefix):
+def rename_fasta(input_fasta, mapfilename, prefix, keep_viral_identifier):
     """Rename a multi-fasta fasta entries with <name>.<counter> and store the
     mapping between new and old files in tsv
     """
     name = os.path.basename(input_fasta).split('.')[0]
-    ext = os.path.basename(input_fasta).split('.')[1]
+    ext = 'fasta'
+    print(ext)
     output = name + '_renamed.' + ext
     map_dir = {}
     print("Renaming " + input_fasta)
@@ -64,12 +69,13 @@ def rename_fasta(input_fasta, mapfilename, prefix):
                 line = str(line)
                 if line.startswith(">"):
                     count += 1
-                    temporary_name = f"{prefix}_{count}"
+                    temporary_name = f"{prefix}{count}"
                     name = line.strip().replace('>', '')
                     viral_identifier = None
-                    if '|' in name:
-                        viral_identifier = name.split('|')[1]
-                        temporary_name = temporary_name + '|' + viral_identifier
+                    if keep_viral_identifier:
+                        if '|' in name:
+                            viral_identifier = name.split('|')[1]
+                            temporary_name = temporary_name + '|' + viral_identifier
                     short_name = name.split(' ')[0]
                     fasta_out.write(f">{temporary_name}\n")
                     tsv_map.writerow([name, temporary_name, short_name])
@@ -77,8 +83,8 @@ def rename_fasta(input_fasta, mapfilename, prefix):
                     map_dir[map_key] = temporary_name
                 else:
                     fasta_out.write(line)
-    print(f"Wrote {count} sequences to {output}.")
-    return map_dir
+    print(f"Wrote {count} sequences to {output}")
+    return map_dir, count
 
 
 def rename_gff(input_gff, map_dir):
@@ -86,8 +92,9 @@ def rename_gff(input_gff, map_dir):
     mapping between new and old files in tsv
     """
     name = os.path.basename(input_gff).split('.')[0]
-    ext = os.path.basename(input_gff).split('.')[1]
+    ext = 'gff'
     output = name + '_renamed.' + ext
+    pattern = r"^(MGYG\d+_\d+)\|([\w.]+)-(\d+):(\d+)$"
 
     print("Renaming " + input_gff)
     with fileinput.hook_compressed(input_gff, "r") as file_in:
@@ -96,6 +103,7 @@ def rename_gff(input_gff, map_dir):
             for line in file_in:
                 if '##' in line:
                     file_out.write(line)
+                    count += 1
                     continue
                 line = line.strip().split('\t')
                 if len(line) == 9:
@@ -105,22 +113,35 @@ def rename_gff(input_gff, map_dir):
                     else:
                         attrs, _ = parse_attrs(line[8])
                         id = attrs.get("ID", "")
+                        id_fna = id
                         full_line = '\t'.join(line)
-                        if id in map_dir:
-                            new_line = full_line.replace(id, map_dir[id])
+                        if re.fullmatch(pattern, id):
+                            # check case when fna has >MGYG000535629_9 viral_sequence|1:3862
+                            # but gff has MGYG000535629_9|viral_sequence-1:3862
+                            m = re.fullmatch(pattern, id)
+                            id_fna = f"{m.group(1)} {m.group(2)}|{m.group(3)}:{m.group(4)}"
+                        if id_fna in map_dir:
+                            new_line = full_line.replace(id, map_dir[id_fna])
                             file_out.write(new_line + '\n')
                         else:
                             file_out.write(full_line + '\n')
+                        count += 1
                 else:
                     file_out.write('\t'.join(line) + '\n')
                     continue
-    print(f"Wrote {count} sequences to {output}.")
+    print(f"Renamed {count} sequences to {output}")
+    return count
 
 
 def main():
     args = input_args()
-    map_dir = rename_fasta(args.input, args.map, args.prefix)
-    rename_gff(args.gff, map_dir)
+    map_dir, count_fasta = rename_fasta(args.input, args.map, args.prefix, args.keep_viral_identifier)
+    count_gff = rename_gff(args.gff, map_dir)
+    if count_gff == count_fasta:
+        print("Sanity check passed")
+    else:
+        print("Number of renamed records doesn't match. Exit")
+        exit(1)
 
 
 if __name__ == "__main__":
